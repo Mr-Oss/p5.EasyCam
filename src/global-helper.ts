f282814514d75659a583c9501f8db5772573d5cb
Michael Rubanov <michael@lusha.com>
2022-01-10 00:05:57 +0200

    --wip-- [skip ci]

--- src/global-helper.ts
+++ src/global-helper.ts
@@ -1,24 +1,24 @@
 
-/**
- * @submodule Camera
- * @for p5
- */
+// /**
+//  * @submodule Camera
+//  * @for p5
+//  */
 
-if (p5) {
-  /**
-     * p5.EasyCam creator function.
-     * Arguments are optional, and equal to the default EasyCam constructor.
-     * @return {EasyCam} a new EasyCam
-     */
-  p5.prototype.createEasyCam = function(/* p5.RendererGL, {state} */) {
-    let renderer = this._renderer;
-    let args = arguments[0];
+// if (p5) {
+//   /**
+//      * p5.EasyCam creator function.
+//      * Arguments are optional, and equal to the default EasyCam constructor.
+//      * @return {EasyCam} a new EasyCam
+//      */
+//   p5.prototype.createEasyCam = function(/* p5.RendererGL, {state} */) {
+//     let renderer = this._renderer;
+//     let args = arguments[0];
 
-    if (arguments[0] instanceof p5.RendererGL) {
-      renderer = arguments[0];
-      args = arguments[1]; // could still be undefined, which is fine
-    }
+//     if (arguments[0] instanceof p5.RendererGL) {
+//       renderer = arguments[0];
+//       args = arguments[1]; // could still be undefined, which is fine
+//     }
 
-    return new Dw.EasyCam(renderer, args);
-  };
-}
+//     return new Dw.EasyCam(renderer, args);
+//   };
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import p5 from 'p5';
+
 import {Rotation} from './rotation';
 
 /*
